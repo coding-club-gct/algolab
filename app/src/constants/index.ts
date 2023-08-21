66cb74ef0bd053484acf0aaf190d64f732fc44f8
april <joel.sr1024@gmail.com>
2023-08-22 03:57:29 +0530

    update constants

--- app/src/constants/index.ts
+++ app/src/constants/index.ts
@@ -1,2 +1,2 @@
-export const judge0URL = "http://localhost:2358"
-export const directusURL = "http://localhost:8055"
+export const judge0URL = process.env.NEXT_PUBLIC_JUDGE0URL ?? "http://localhost:2358"
+export const directusURL = process.env.NEXT_PUBLIC_DIRECTUSURL ?? "http://localhost:8055"
